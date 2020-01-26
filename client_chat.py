ab3aaeedb9643ad7e8ecafd26992d5090be7ccd9
SouravBera <53810519+SouravBear8850@users.noreply.github.com>
2020-01-26 23:13:14 +0530

    Added Chat-server

---
+++ client_chat.py
@@ -0,0 +1,24 @@
+import socket
+
+
+client_socket=socket.socket()
+port=12345
+client_socket.connect(('127.0.0.2',port))
+
+recv_msg=client_socket.recv(1024)
+print(recv_msg)
+
+send_msg= input("Enter your user name(prefix with #:")
+client_socket.send(send_msg)
+
+
+while True:
+    recv_msg=client_socket.recv(1024)
+    print(recv_msg)
+    send_msg= raw_input("Send your message in format [@user:message] ")
+    if send_msg== 'exit':
+        break;
+    else:
+        client_socket.send(send_msg)
+
+client_socket.close()
